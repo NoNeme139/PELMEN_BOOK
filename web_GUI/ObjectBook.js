41f7709c4a317d7f8f7c7d32407985d974ce650e
NoNeme139 <65152247+NoNeme139@users.noreply.github.com>
2021-01-21 23:03:29 +0300

    efef

--- web_GUI/ObjectBook.js
+++ web_GUI/ObjectBook.js
@@ -26,3 +26,23 @@ function addMyBookTolibrary(anyBookObt){ // Функция которая не �
 
 let book1 = new book("NAME BOOK", "AUTOR BOOK", 45)
 addMyBookTolibrary(book1)
+
+document.getElementById("clearButton").onclick = function(e) {
+	document.getElementById("textInput").value = "";
+	}
+	
+	function eventForm(value){
+		console.log(value)
+		document.body.onload = addElement(value);
+	}
+
+	var my_div = newDiv = null;
+
+	function addElement(anyText) {
+
+		var newDiv = document.createElement("div");
+			newDiv.innerHTML = "<h1>" + anyText + "</h1>";
+
+		my_div = document.getElementById("org_div1");
+		document.body.insertBefore(newDiv, my_div);
+	}
